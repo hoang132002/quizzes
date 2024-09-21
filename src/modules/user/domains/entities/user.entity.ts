f054bb9bb2ff0d256bb7b0f8a53875201c3a305d
zang <bt.hoanggiang@gmail.com>
2024-09-21 22:59:30 +0700

    feat: add field for user  entity

---
+++ src/modules/user/domains/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { AnswerEntity } from 'src/modules/answer/domains/entities/answer.entity';
+import { SubmissionEntity } from 'src/modules/submission/domains/entities/submission.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'user' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => AnswerEntity, (answer) => answer.user)
+  answers: AnswerEntity[];
+
+  @OneToMany(() => SubmissionEntity, (submission) => submission.user)
+  submissions: SubmissionEntity[];
+}
